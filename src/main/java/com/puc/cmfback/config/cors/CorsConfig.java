4c86fb52a597e2dbf1c4b7627cb541b26657f6ff
Guilherme Ferreira <guilherme.rfs00@gmail.com>
2021-09-23 13:36:25 -0300

    Cors config

--- src/main/java/com/puc/cmfback/config/cors/CorsConfig.java
+++ src/main/java/com/puc/cmfback/config/cors/CorsConfig.java
@@ -1,19 +1,22 @@
-package com.puc.cmfback.config;
+package com.puc.cmfback.config.cors;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 
 
 @Configuration
-public class AppConfig {
+@EnableWebMvc
+public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -29,10 +32,15 @@ public class AppConfig {
     }
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> bean
-                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.GET);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
         return bean;
     }
 }

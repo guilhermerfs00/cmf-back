20d8589063ace92e33a5e0eacc03c8667281dd50
Guilherme Ferreira <guilherme.rfs00@gmail.com>
2021-09-26 18:24:03 -0300

    Content type Json no Cors

--- src/main/java/com/puc/cmfback/config/CORSFilter.java
+++ src/main/java/com/puc/cmfback/config/CORSFilter.java
@@ -25,7 +25,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:8100");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Auth-Token");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Auth-Token, Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if(!"OPTIONS".equalsIgnoreCase(request.getMethod())) {
